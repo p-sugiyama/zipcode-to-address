479a6b9e48a6c1c30333c54e3c698aa4f3340fe8
p-sugiyama <51998935+p-sugiyama@users.noreply.github.com>
2019-07-07 16:04:15 +0900

    ツールボックでコンポーネントを配置

--- zipcode-to-address/Form1.cs
+++ zipcode-to-address/Form1.cs
@@ -16,5 +16,15 @@ namespace zipcode_to_address
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
